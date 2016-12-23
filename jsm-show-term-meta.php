94646eaafd782131509dd1f000b604a2e773e6b8
JS Morisset <jsm@surniaulula.com>
2016-12-22 18:15:31 -0800

    v1.0.1-dev1

--- jsm-show-term-meta.php
+++ jsm-show-term-meta.php
@@ -44,6 +44,7 @@ if ( ! class_exists( 'JSM_Show_Term_Meta' ) ) {
 	class JSM_Show_Term_Meta {
 
 		private static $instance;
+		private static $wp_min_version = 4.4;
 	
 		public $view_cap;
 		public $tax_slug;
@@ -56,13 +57,34 @@ if ( ! class_exists( 'JSM_Show_Term_Meta' ) ) {
 	
 		private function __construct() {
 			if ( is_admin() ) {
+				load_plugin_textdomain( 'jsm-show-term-meta', false, 'jsm-show-term-meta/languages/' );
+
+				add_action( 'admin_init', array( __CLASS__, 'check_wp_version' ) );
+
 				// make sure we have a taxonomy slug to hook the metabox action
 				if ( ( $this->tax_slug = $this->get_request_value( 'taxonomy' ) ) !== '' )
-					add_action( $this->tax_slug.'_edit_form', 
-						array( &$this, 'show_meta_boxes' ), 1000, 1 );
+					add_action( $this->tax_slug.'_edit_form', array( &$this, 'show_meta_boxes' ), 1000, 1 );
 			}
 		}
 	
+		public static function check_wp_version() {
+			global $wp_version;
+			if ( version_compare( $wp_version, self::$wp_min_version, '<' ) ) {
+				$plugin = plugin_basename( __FILE__ );
+				if ( is_plugin_active( $plugin ) ) {
+					require_once( ABSPATH.'wp-admin/includes/plugin.php' );	// just in case
+					$plugin_data = get_plugin_data( __FILE__, false );	// $markup = false
+					deactivate_plugins( $plugin );
+					wp_die( 
+						sprintf( __( '%1$s requires WordPress version %2$s or higher and has been deactivated.',
+							'jsm-show-term-meta' ), $plugin_data['Name'], self::$wp_min_version ).'<br/><br/>'.
+						sprintf( __( 'Please upgrade WordPress before trying to reactivate the %1$s plugin.',
+							'jsm-show-term-meta' ), $plugin_data['Name'] )
+					);
+				}
+			}
+		}
+
 		public function show_meta_boxes( $term_obj ) {
 			if ( ! isset( $term_obj->term_id ) )	// just in case
 				return;
@@ -73,10 +95,10 @@ if ( ! class_exists( 'JSM_Show_Term_Meta' ) ) {
 				! apply_filters( 'jsm_stm_taxonomy', true, $term_obj->taxonomy ) )
 					return;
 	
-			add_meta_box( 'jsm-stm', 'Term Meta', 
+			add_meta_box( 'jsm-stm', __( 'Term Meta', 'jsm-show-term-meta' ),
 				array( &$this, 'show_term_meta' ), 'jsm-stm-term', 'normal', 'low' );
 	
-			echo '<h3 id="jsm-stm-metaboxes">Show Term Meta</h3>';
+			echo '<h3 id="jsm-stm-metaboxes">'.__( 'Show Term Meta', 'jsm-show-term-meta' ).'</h3>';
 			echo '<div id="poststuff">';
 			do_meta_boxes( 'jsm-stm-term', 'normal', $term_obj );
 			echo '</div><!-- .poststuff -->';
@@ -89,10 +111,7 @@ if ( ! class_exists( 'JSM_Show_Term_Meta' ) ) {
 			$term_meta = apply_filters( 'jsm_stm_term_meta', 
 				get_term_meta( $term_obj->term_id ), $term_obj );	// since wp v3.0
 	
-			$skip_keys = apply_filters( 'jsm_stm_skip_keys', 
-				array(
-				)
-			);
+			$skip_keys = apply_filters( 'jsm_stm_skip_keys', array() );
 	
 			?>
 			<style>
@@ -115,9 +134,10 @@ if ( ! class_exists( 'JSM_Show_Term_Meta' ) ) {
 					width:20%;
 				}
 			</style>
-			<table><thead><tr><th class="key-column">Key</th>
-			<th class="value-column">Value</th></tr></thead><tbody>
 			<?php
+	
+			echo '<table><thead><tr><th class="key-column">'.__( 'Key', 'jsm-show-term-meta' ).'</th>';
+			echo '<th class="value-column">'.__( 'Value', 'jsm-show-term-meta' ).'</th></tr></thead><tbody>';
 	
 			ksort( $term_meta );
 			foreach( $term_meta as $key => $arr ) {
