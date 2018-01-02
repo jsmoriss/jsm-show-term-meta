757a64532ddc719166ffdd2b836f6b91ce3ef3b4
JS Morisset <jsm@surniaulula.com>
2018-01-02 08:53:08 -0600

    v1.0.4

--- jsm-show-term-meta.php
+++ jsm-show-term-meta.php
@@ -70,8 +70,8 @@ if ( ! class_exists( 'JSM_Show_Term_Meta' ) ) {
 					if ( ! function_exists( 'deactivate_plugins' ) ) {
 						require_once trailingslashit( ABSPATH ) . 'wp-admin/includes/plugin.php';
 					}
-					$plugin_data = get_plugin_data( __FILE__, false );	// $markup = false
-					deactivate_plugins( $plugin, true );	// $silent = true
+					$plugin_data = get_plugin_data( __FILE__, false ); // $markup = false
+					deactivate_plugins( $plugin, true ); // $silent = true
 					wp_die( 
 						'<p>' . sprintf( __( '%1$s requires %2$s version %3$s or higher and has been deactivated.',
 							'jsm-show-term-meta' ), $plugin_data['Name'], 'WordPress', $wp_min_version ) . '</p>' . 
