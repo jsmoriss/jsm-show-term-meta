805c3e11ae1824488e26722b181195c897c305e5
JS Morisset <jsm@surniaulula.com>
2020-01-16 07:02:08 -0800

    v1.1.0

--- jsm-show-term-meta.php
+++ jsm-show-term-meta.php
@@ -13,7 +13,7 @@
  * Requires PHP: 5.5
  * Requires At Least: 4.4
  * Tested Up To: 5.3.2
- * Version: 1.0.4
+ * Version: 1.1.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
