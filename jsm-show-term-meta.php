e42081927d97325214934ae34c561696545b99b1
JS Morisset <jsm@surniaulula.com>
2021-04-25 15:18:46 -0700

    v1.3.0

--- jsm-show-term-meta.php
+++ jsm-show-term-meta.php
@@ -157,7 +157,7 @@ if ( ! class_exists( 'JSM_Show_Term_Metadata' ) ) {
 			$skip_keys_preg_match = apply_filters( 'jsm_stm_skip_keys', array() );
 
 			?>
-			<style>
+			<style type="text/css">
 				div#jsm-stm.postbox table {
 					width:100%;
 					max-width:100%;
