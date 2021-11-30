70bb089beaf9755911480803456ef2073f0d3434
JS Morisset <jsm@surniaulula.com>
2021-11-30 13:13:23 -0800

    v3.0.0-rc.1

--- lib/config.php
+++ lib/config.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'JsmStmConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'jsmstm' => array(			// Plugin acronym.
-					'version'     => '3.0.0-b.1',	// Plugin version.
+					'version'     => '3.0.0-rc.1',	// Plugin version.
 					'slug'        => 'jsm-show-term-meta',
 					'base'        => 'jsm-show-term-meta/jsm-show-term-meta.php',
 					'text_domain' => 'jsm-show-term-meta',
