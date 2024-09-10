7c1bd4ca4a9bdf7caf559f4c836d8ddf2faad1dd
jsmoriss <jsm@surniaulula.com>
2024-09-10 18:30:16 +0000

    v4.6.0

--- jsm-show-term-meta.php
+++ jsm-show-term-meta.php
@@ -12,7 +12,7 @@
  * Description: Show term metadata in a metabox when editing terms - a great tool for debugging issues with term metadata.
  * Requires PHP: 7.2.34
  * Requires At Least: 5.8
- * Tested Up To: 6.6.1
+ * Tested Up To: 6.6.2
  * Version: 4.6.0
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
